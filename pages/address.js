fa545c700c15df86bc69ef4e854bb906ac92d899
logologa4 <logologa4@Logologa4.local>
2020-08-07 15:53:59 +0200

    Initial commit.

--- pages/address.js
+++ pages/address.js
@@ -16,9 +16,8 @@ var address = '0x004871f4ca28c304a7bed2e6c14a98e8837d1dc673108fe7dc';
 
 
 
-function Home({balanceResult}) {
+function Address(balanceResult) {
  
- 	var linkec = "https://venus.mhscan.com/?page=address&id=" + balanceResult.address;
   console.log(balanceResult.address);
   return (
     <div className="container">
@@ -29,27 +28,30 @@ function Home({balanceResult}) {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js </a>& <a href="https://nextjs.org">Metahash-js API</a>!
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
-          Get started by reading pages on <code><a href="http://www.metahash.org" target="_blank">metahash.org</a></code>
+          Get started by editing <code>pages/index.js</code>
         </p>
 
         <div className="grid">
-          <a href={linkec} target="_blank" className="card">
-            <h3>Wallet Address &rarr;</h3>
+          <a href="https://nextjs.org/docs" className="card">
+            <h3>Documentation &rarr;</h3>
             <p>{  balanceResult.address  }</p>
           </a>
 
-          <a href={linkec} target="_blank" className="card">
-            <h3>Wallet Balance &rarr;</h3>
-            <p>{ ( balanceResult.received -  balanceResult.spent) / 1e6 }</p>
+          <a href="https://nextjs.org/learn" className="card">
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a href={linkec} target="_blank" className="card">
-            <h3>Wallet Delegation &rarr;</h3>
-            <p>{ ( balanceResult.delegate -  balanceResult.undelegate) / 1e6 }</p>
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className="card"
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
           <a
@@ -132,8 +134,7 @@ function Home({balanceResult}) {
         .title {
           margin: 0;
           line-height: 1.15;
-          font-size: 3rem;
-          wrap: break-word;
+          font-size: 4rem;
         }
 
         .title,
@@ -215,7 +216,6 @@ function Home({balanceResult}) {
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
-            overflow-wrap: break-word;
         }
 
         * {
@@ -251,4 +251,4 @@ export async function getServerSideProps() {
 
 
 
-export default Home
+export default Address
